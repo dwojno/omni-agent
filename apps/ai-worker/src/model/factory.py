cfe5ff1d5af30bc45727483f2021f1a7f13c9517
Daniel Wojno <daniel.wojno@energyweb.org>
2026-02-18 11:05:34 +0100

    feat: add llm model factory

---
+++ apps/ai-worker/src/model/factory.py
@@ -0,0 +1,54 @@
+from typing import Literal
+
+from llama_index.core.embeddings import BaseEmbedding
+from llama_index.core.llms import LLM
+from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.gemini import Gemini
+from llama_index.llms.openai import OpenAI
+
+ModelProvider = Literal["openai", "gemini"] | str
+
+
+class LLMFactory:
+    @staticmethod
+    def create_llm(provider: ModelProvider, temperature: float = 0) -> LLM:
+        if provider == "gemini":
+            return Gemini(
+                model="models/gemini-1.5-pro",
+                temperature=temperature,
+                api_key="",
+                safety_settings=[
+                    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+                    {
+                        "category": "HARM_CATEGORY_HATE_SPEECH",
+                        "threshold": "BLOCK_NONE",
+                    },
+                    {
+                        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                        "threshold": "BLOCK_NONE",
+                    },
+                    {
+                        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                        "threshold": "BLOCK_NONE",
+                    },
+                ],
+            )
+        elif provider == "openai":
+            return OpenAI(
+                model="gpt-4o",
+                temperature=temperature,
+                api_key="",
+            )
+        else:
+            raise ValueError(f"Unknown provider: {provider}")
+
+    @staticmethod
+    def create_embedding(provider: ModelProvider) -> BaseEmbedding:
+        if provider == "gemini":
+            return GeminiEmbedding(model_name="models/text-embedding-004")
+
+        elif provider == "openai":
+            return OpenAIEmbedding(model="text-embedding-3-small")
+        else:
+            raise ValueError(f"Unknown provider: {provider}")
